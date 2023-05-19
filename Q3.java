30b81f5eec93b5f39bf449b554acc2c26382a149
Vking4 <95952614+Vking4@users.noreply.github.com>
2023-05-19 13:51:11 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args){
+        int r=22;
+        System.out.println("Perimeter is"+2*(22/7)*r);
+        System.out.println("Area is"+(22/7)*r*r);
+    }
+    
+}
