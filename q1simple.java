c4a4bb0569c5542dc6c41cb4a262ddf9059a829b
Vking4 <95952614+Vking4@users.noreply.github.com>
2023-05-19 13:53:47 +0530

    Add files via upload

---
+++ q1simple.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class q1simple {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Hello, VIT");
+        Label label = new Label("Hello, VIT");
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
