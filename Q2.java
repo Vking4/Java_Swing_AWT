a510d8e8d37497b3fd4dc13f7ea6c22e634630ca
Vking4 <95952614+Vking4@users.noreply.github.com>
2023-05-19 13:50:48 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,13 @@
+public class Q2 {
+    public static void main(String[] args){
+        int a=32;
+        int b=22;
+        System.out.println( a+b);
+        System.out.println( a-b);
+        System.out.println( a*b);
+        System.out.println( a/b);
+        System.out.println( a%b);
+
+    }
+    
+}
